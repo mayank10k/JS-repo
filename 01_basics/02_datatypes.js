04c62b555241839ab800aba98b82efbb412a52c0
Mayank Mehta <mayankmehta433@gmail.com>
2025-08-15 14:52:55 +0530

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using node js noot browser
+
+let name="mayank"
+let age=45;
+let isLoggedIn= true
+
+
+//  PRIMITIVE DATA TYPE:
+
+//number --> range 2 to the power 53
+//bigint --> for very large value
+//string --> use ""
+//boolean --> true/false
+//null --> stand alone value / it is a type / representation of empty value
+//undefined --> variable is decleared but any value is not assigned
+//symbol -->for unique ness
+
+
+//object
+
+console.log(typeof"mayank");
+console.log(typeof 54);
+console.log(typeof null);  //Output: object
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
