1794ca5e75a476c68446813d9459e04ea5c06c1d
Mayank Mehta <mayankmehta433@gmail.com>
2025-08-15 15:18:16 +0530

    update in DT

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,16 +6,19 @@ let name="mayank"
 let age=45;
 let isLoggedIn= true
 
+/*
+ PRIMITIVE DATA TYPE:  first letter of all the data types should be in upper case while we are using the data type key word
+                           
 
-//  PRIMITIVE DATA TYPE:
+number --> range 2 to the power 53
+bigint --> for very large value
+string --> use ""
+boolean --> true/false
+null --> stand alone value / it is a type / representation of empty value
+undefined --> variable is decleared but any value is not assigned
+symbol -->for unique ness
 
-//number --> range 2 to the power 53
-//bigint --> for very large value
-//string --> use ""
-//boolean --> true/false
-//null --> stand alone value / it is a type / representation of empty value
-//undefined --> variable is decleared but any value is not assigned
-//symbol -->for unique ness
+*/
 
 
 //object
