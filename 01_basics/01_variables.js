1f8488b24ff12bd5dc8c0e4f763f851f1cc9997a
Mayank Mehta <mayankmehta433@gmail.com>
2025-08-15 14:51:57 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountid =3455
+let accountemail="mayankmehta@gmail.com"
+var accountpassword="jaipur"
+accountcity="jaipur"
+let accState;
+
+// accountid=2   //const cann't be changed
+
+accountemail="mayank"
+accountpassword="3434"
+accountcity="almora"
+
+console.log(accountid);
+// console.log('');
+
+/*
+
+*/
+
+console.table([accountid,accountemail,accountpassword,accountcity,accState])
+
