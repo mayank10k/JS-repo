73b2ccb819b53838dc5c201cc7d9b05496062360
Mayank Mehta <mayankmehta433@gmail.com>
2025-08-16 14:42:41 +0530

    conversion and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -5,16 +5,16 @@ let score=true
 
 let a="45ab"
 
-console.log(typeof score)
-console.log(typeof a)
+// console.log(typeof score)
+// console.log(typeof a)
 
 let valinNum=Number(score)
 let Num=Number(a)
 
-console.log(typeof valinNum)
+// console.log(typeof valinNum)
 
-console.log(typeof Num)  // it will be converted to number but 
-console.log(Num)         // but in actual it is not a number   // Output NaN
+// console.log(typeof Num)  // it will be converted to number but 
+// console.log(Num)         // but in actual it is not a number   // Output NaN
 
 
 
@@ -35,10 +35,50 @@ true/false --> 1/0
 // let val="mayank"
 let val=""
 let newVal=Boolean(val)
-console.log(newVal)
+// console.log(newVal)
 
 
 /*
 1--> true , 0--> false
 "abc" --> ture , ""--> false
 */
+
+//**************************** Operations *****************************************
+
+
+let n=3
+let N=-n
+// console.log(N)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+
+let str1="mayank"
+let str2="mehta"
+// console.log(str1+str2);
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); // agar string 1st h toh sbhi ko string ki trh treat kiya jayega
+// console.log(1+2+"2");  // agar string last m toh phele jo vo conversionn ho jayega then string bna diya jayega
+
+
+
+// console.log(+true)  //-->1
+// console.log(+"");   //-->0
+
+let num1,num2,num3
+
+num1=num2=num3= 2+2  //don't use such way
+
+
+
+
