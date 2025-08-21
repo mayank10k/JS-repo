4f4254e482597c5f371d90fe58d50b15833993ef
Mayank Mehta <mayankmehta433@gmail.com>
2025-08-21 15:00:04 +0530

    string

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,16 @@
+const name="mayak"
+const num=69;
+
+// console.log(name + num);     --> don't use this method
+
+// string inter-polation
+console.log(`hello my name is ${name} and my roll no. is ${num}`) // --->use this instead
+
+//one more type to intialise a string
+
+const game= new String("mayank")
+console.log(game);
+
+console.log(game.toUpperCase());
+console.log(game.charAt(3));
+console.log(game.indexOf('a'));
